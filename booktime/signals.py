c47738fc6b3eaec79e0fb1b15d289101f35a4527
justvladick <justvladick@gmail.com>
2019-03-24 12:31:33 +0300

    v.04 (24.03.19) with root path
    Signed-off-by: justvladick <justvladick@gmail.com>

--- booktime/signals.py
+++ booktime/signals.py
@@ -39,26 +39,26 @@ def generate_thumbnail(sender, instance, **kwargs):
 @receiver(user_logged_in)
 def merge_baskets_if_found(sender, user, request, **kwargs):
     anonymous_basket = getattr(request, "basket", None)
-    if anonymous_basket:
-        try:
-            loggedin_basket = Basket.objects.get(
-                user=user, status=Basket.OPEN
-            )
-            for line in anonymous_basket.basketline_set.all():
-                line.basket = loggedin_basket
-                line.save()
-            anonymous_basket.delete()
-            request.basket = loggedin_basket
-            logger.info(
-                "Merged basket to id %d", loggedin_basket.id
-            )
-        except Basket.DoesNotExist:
-            anonymous_basket.user = user
-            anonymous_basket.save()
-            logger.info(
-                "Assigned user to basket id %d",
-                anonymous_basket.id,
-            )
+    # if anonymous_basket:
+    #     try:
+    #         loggedin_basket = Basket.objects.get(
+    #             user=user, status=Basket.OPEN
+    #         )
+    #         for line in anonymous_basket.basketline_set.all():
+    #             line.basket = loggedin_basket
+    #             line.save()
+    #         anonymous_basket.delete()
+    #         request.basket = loggedin_basket
+    #         logger.info(
+    #             "Merged basket to id %d", loggedin_basket.id
+    #         )
+    #     except Basket.DoesNotExist:
+    #         anonymous_basket.user = user
+    #         anonymous_basket.save()
+    #         logger.info(
+    #             "Assigned user to basket id %d",
+    #             anonymous_basket.id,
+    #         )
 
 
 @receiver(post_save, sender=OrderLine)
